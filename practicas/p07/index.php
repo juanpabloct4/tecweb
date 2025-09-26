06fe84932bcb60bf89e7d31fb4cb463c735c0834
juanpabloct4 <juanpabloct4@gmail.com>
2025-09-25 19:41:00 -0600

    Ejercicios 2 y 3 agregados al index.php

--- practicas/p07/index.php
+++ practicas/p07/index.php
@@ -6,37 +6,58 @@
     <title>Práctica 4</title>
 </head>
 <body>
+    <?php
+        require_once 'src/funciones.php';
+    ?>
+
     <h2>Ejercicio 1</h2>
     <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
     <?php
-        if(isset($_GET['numero']))
-        {
-            $num = $_GET['numero'];
-            if ($num%5==0 && $num%7==0)
-            {
-                echo '<h3>R= El número '.$num.' SÍ es múltiplo de 5 y 7.</h3>';
-            }
-            else
-            {
-                echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
-            }
-        }
+        Multiplo5y7()
     ?>
 
-    <h2>Ejemplo de POST</h2>
+    <!-- <h2>Ejemplo de POST</h2>
     <form action="http://localhost/tecweb/practicas/p07/index.php" method="post">
         Name: <input type="text" name="name"><br>
         E-mail: <input type="text" name="email"><br>
         <input type="submit">
     </form>
-    <br>
+    <br>-->
     <?php
-        if(isset($_POST["name"]) && isset($_POST["email"]))
-        {
-            echo $_POST["name"];
-            echo '<br>';
-            echo $_POST["email"];
-        }
+        // if(isset($_POST["name"]) && isset($_POST["email"]))
+        // {
+        //     echo $_POST["name"];
+        //     echo '<br>';
+        //     echo $_POST["email"];
+        // }
+    ?> 
+
+    <!-- Comentar Ctrl + K + Ctrl + C
+    Descomentar Ctrl + K + Ctrl + U -->
+
+    <hr>
+
+    <h2>Ejercicio 2</h2>
+    <p>Crea un programa para la generación repetitiva de 3 número aleatorios hasta obtener
+        una secuencia compueta por:
+        <span style="color:blue;">impar</span>, 
+        <span style="color:orange;">par</span>, 
+        <span style="color:blue;">impar</span>.
+    </p>
+    <?php
+        ImparParImpar()
     ?>
+
+    <hr>
+
+    <h2>Ejercicio 3</h2>
+    <p>Utiliza un ciclo while para encontrar el primer número entero obtenido aleatoriamente,
+        pero que además sea múltiplo de un número dado.</p>
+    <?php
+        MultiploWhile();
+        echo "<p>Usando Do - While: </p>";
+        MultiploDoWhile();
+    ?>
+    
 </body>
 </html>
